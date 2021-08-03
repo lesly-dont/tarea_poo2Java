39cf102c2a0fa89e34c327075a20ad103288ea03
lesly-dont-exe <leslyqs016@gmail.com>
2021-08-02 21:49:38 -0600

    tarea2_pooJava

---
+++ src/vista2/main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista2;
+
+/**
+ *
+ * @author 50254
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frm_cliente frmc = new frm_cliente();
+        frmc.show();
+        
+        // frm_empleado frme = new frm_empleado ();
+        // frme.show ();
+    }
+    
+}
